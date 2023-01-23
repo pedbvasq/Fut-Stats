c43c1d42c70ef990ab9238efc3db7f4543e06e03
aaalfran <aaalfran@espol.edu.ec>
2023-01-22 21:41:22 -0500

    League backend update

--- footbackend/routes/stats.js
+++ footbackend/routes/stats.js
@@ -12,6 +12,8 @@ router.get('/', function(req, res, next) {
 });
 
 
+//TEAMS: Pedro Bajaña
+
 router.get('/teams',function(req,res,next){
   Teams.findAll().
   then(data=>{
@@ -114,6 +116,11 @@ router.put('/teams/:idteams', function(req, res, next){
       });
     });
 });
+
+
+
+//LEAGUE: Aaron Franco
+
 router.get('/league',function(req,res,next){
   League.findAll().
   then(data=>{
@@ -122,13 +129,79 @@ router.get('/league',function(req,res,next){
     console.log(e)
 
   })
-}
-)
+});
 
 
+router.post('/league/', function(req, res, next) {
+  // Validate request
+  if (!req.body.nameleague) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+    return;
+  }
+
+  const leagues = {
+    idleague :req.body.idleague,
+    leaguename: req.body.leaguename,
+    budget : req.body.budget
+  };
 
 
+  League.create(league)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Tutorial."
+      });
+    });
+})
 
+router.put('/league/:idleague', function(req, res, next){
+  let idP = req.params.idleague
+
+  Tutorial.update(req.body, {
+    where: {  idleague:idP}
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Leagues was updated successfully."
+        });
+      } else {
+        res.send({
+          message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found or req.body is empty!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Tutorial with id=" + id
+      });
+    });
+});
+
+router.delete('/league/:idleague', function(req, res, next) {
+  let idP = req.params.idleague
+   League.findAll({
+      where:{
+        idleague:idP
+      }
+   }).
+   then(data =>{
+    console.log(data)
+    res.send(data);
+   })
+   .catch(e=>{
+    console.log(e)
+   })
+ })
+
+
+//PLAYERS: Fabrizzio Ontaneda
  router.get('/players', function(req, res, next) {
   Players.findAll().
   then(data=>{
