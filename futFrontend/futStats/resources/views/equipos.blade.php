c5dbb4e1850dc77004480419eebf25f8783c5329
aaalfran <aaalfran@espol.edu.ec>
2023-01-22 22:48:32 -0500

    Pestana teams

--- futFrontend/futStats/resources/views/equipos.blade.php
+++ futFrontend/futStats/resources/views/equipos.blade.php
@@ -1,21 +1,23 @@
 @include('partials.navbar')
-<div>
-    <table class="table table-striped table-dark">
-        <thead>
-            <tr>
-                <th scope="col">nombre</th>
-                <th scope="col">puntos</th>
-                <th scope="col">titulos</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($data as $equipo)
-            <td> {{$equipo['nameteam']}}</td>
-            <td> {{$equipo['points']}}</td>
-            <td> {{$equipo['titles']}}</td>
-            @endforeach
-        </tbody>
-    
-    </table>
-</div>
-
+<table class="table table-striped table-dark">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Presupuesto</th>
+      <th scope="col">Puntos</th>
+      <th scope="col">Titulos</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach ($data as $equipo)
+      <tr>
+        <td> {{$equipo['idteams']}}</td>
+        <td> {{$equipo['nameteam']}}</td>
+        <td> {{$equipo['budget']}}</td>
+        <td> {{$equipo['points']}}</td>
+        <td> {{$equipo['titles']}}</td>
+      </tr>
+      @endforeach
+  </tbody>
+</table>
